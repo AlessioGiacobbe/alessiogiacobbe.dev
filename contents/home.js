b88f31e012bfc51414d99d914efe6c62dc9fcd0f
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-06-16 17:32:08 +0200

    added content mapping

--- contents/home.js
+++ contents/home.js
@@ -1,14 +1,74 @@
 export const sections = [
     {
-        name: "things_i_do"
+        name: "things_i_do",
+        items: [
+            {
+                "title" : "React",
+                "description" : ""
+            },
+            {
+                "title" : "Laravel",
+                "description" : ""
+            },
+            {
+                "title" : "Symfony",
+                "description" : ""
+            }
+        ]
     },
     {
-        name: "things_im_learning"
+        name: "things_im_learning",
+        items: [
+            {
+                "title" : "Docker",
+                "description" : ""
+            },
+            {
+                "title" : "Kubernetes",
+                "description" : ""
+            }
+        ]
     },
     {
-        name: "things_i_did"
+        name: "things_i_did",
+        items: [
+            {
+                "title" : "betamicroprocessor",
+                "description" : ""
+            },
+            {
+                "title" : "isaac_leaderboard",
+                "description" : ""
+            },
+            {
+                "title" : "this_website",
+                "description" : ""
+            },
+            {
+                "title" : "more",
+                "description" : "coming soon"
+            }
+        ]
     },
     {
-        name: "things_i_like"
+        name: "things_i_like",
+        items: [
+            {
+                "title" : "Programming",
+                "description" : ""
+            },
+            {
+                "title" : "Gaming",
+                "description" : ""
+            },
+            {
+                "title" : "Cooking",
+                "description" : ""
+            },
+            {
+                "title" : "Space",
+                "description" : ""
+            },
+        ]
     }
 ]
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import useTranslation from 'next-translate/useTranslation';
+import { sections } from '../contents/home';
 
 export default function Home() {
   const { t } = useTranslation('common');
 
   return (
     <div className={styles.container}>
-      <div>{t('ciao')}</div>
+      {
+        sections.map(section => {
+          return <div key={section.name} className='Section'>
+            {
+              section.items.map(item => {return <div key={item.title} className='section-item'>
+                {item.title}
+              </div>})
+            }
+          </div>
+        })
+      }
     </div>
   )
 }
