3a10bf000e4be60253b99c9ae6f00ca8f52c96fd
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-06-16 17:00:45 +0200

    added translations with next-translate

---
+++ contents/home.js
@@ -0,0 +1,14 @@
+export const sections = [
+    {
+        name: "things_i_do"
+    },
+    {
+        name: "things_im_learning"
+    },
+    {
+        name: "things_i_did"
+    },
+    {
+        name: "things_i_like"
+    }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useTranslation from 'next-translate/useTranslation';
 
 export default function Home() {
+  const { t } = useTranslation('common');
+
   return (
     <div className={styles.container}>
-      
+      <div>{t('ciao')}</div>
     </div>
   )
 }
