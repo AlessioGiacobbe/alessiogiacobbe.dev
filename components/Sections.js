fd804927e64d382960cc954fcd499f5eae2d15fd
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-06-18 23:12:38 +0200

    sections components

--- components/Sections.js
+++ components/Sections.js
@@ -4,14 +4,23 @@ function Sections() {
     return <>
         {
             sectionsItems.map(section => {
-                return <div key={section.name} className='Section'>
-                    {
-                        section.items.map(item => {
-                            return <div key={item.title} className='section-item'>
-                                {item.title}
-                            </div>
-                        })
-                    }
+                return <div key={section.name} className='grid grid-cols-2  gap-4'>
+                    <div className='col-span-3 row-span-3 bg-yellow-400'>
+                        {section.name}
+                    </div>
+                    <div className="flex overflow-x-scroll pb-10 hide-scroll-bar col-span-2">
+                        <div className="flex flex-nowrap ">
+                            {
+                                section.items.map(item => {
+                                    return <div className="inline-block px-3">
+                                        <div key={item.title} className="w-64 h-32 rounded-lg shadow-md bg-white">
+                                            {item.title}
+                                        </div>
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             })
         }
