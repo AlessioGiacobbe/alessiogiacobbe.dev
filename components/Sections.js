16456ed42a5a2b7a0f5ecc61935fa01769e75131
Alessio Giacobbe <alessiogiacobbe@MacBook-Pro-di-Alessio.local>
2022-06-20 00:21:17 +0200

    Sections components refactor

--- components/Sections.js
+++ components/Sections.js
@@ -3,9 +3,9 @@ import Section from './Section';
 
 function Sections() {
     return <>
-        <div className='mx-auto  justify-center px-8 left-1/2  transform px-8 max-w-6xl'>
+        <div className='mx-auto justify-center px-8 left-1/2  transform px-8 max-w-6xl'>
             {
-                sectionsItems.map(section => <Section name={section.name} items={section.items} />)
+                sectionsItems.map(section => <Section key={section.name} name={section.name} items={section.items} />)
             }
         </div>
     </>
